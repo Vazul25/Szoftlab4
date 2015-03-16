41908d78e6186231e4a2adb2fe6adda30db8e24f
demegis <tht_krisztian@sch.bme.hu>
2015-03-16 18:21:09 +0100

    Comment #4

---
+++ src/minor/MyTimer.java
@@ -0,0 +1,34 @@
+﻿package minor;
+
+/*
+ * Timer osztály
+ * Felelősség:
+ * 
+ * Ősosztály:
+ * 
+ * Interfészek:
+ *  
+ */
+public class MyTimer {
+	/*
+	 * Timer konstruktor
+	 * ha 0-val inicializálják felfele számol
+	 * ha pozitív számmal inicializálják akkor lefelé számol.
+	 */
+	public MyTimer(int i){
+		
+	}
+	
+	public void start(){
+		
+	}
+	
+	public boolean isZero(){
+		return false;
+		
+	}
+	
+	public int getTime(){
+		return 0;		
+	};
+}
