fa00ae11faaa8bd67ac3aa460cbf11643d083478
demegis <tht_krisztian@sch.bme.hu>
2015-03-29 17:49:04 +0200

    List<Cleaner> a Phoebe-ben + MyTimer imp

--- src/minor/MyTimer.java
+++ src/minor/MyTimer.java
@@ -1,5 +1,7 @@
 ﻿package minor;
 
+import java.math.BigDecimal;
+
 /*
  * Timer osztály
  * Felelősség:
@@ -14,25 +16,56 @@
  *  
  */
 public class MyTimer {
+	
+	private long T_start;
+	private int duration;
+	
+	private enum DIR{
+		Up,
+		Down
+	}
+	
+	private DIR direction;
+	
 	/*
 	 * Timer konstruktor
-	 * ha 0-val inicializálják felfele számol
+	 * ha 0-val vagy negatívval inicializálják felfele számol
 	 * ha pozitív számmal inicializálják akkor lefelé számol.
 	 */
 	public MyTimer(int i){
-		
+		if(i > 0){
+			direction = DIR.Down;
+			duration = i*1000;
+			T_start = System.currentTimeMillis()-2*i*1000;
+		}else{
+			direction = DIR.Up;
+			duration = 0;
+		}
 	}
 	
 	public void start(){
+		switch (direction){
+		case Down:
+			if( T_start + duration < System.currentTimeMillis() ) T_start = System.currentTimeMillis();
+			break;
+		default:
+			T_start = System.currentTimeMillis();
+			break;
+		}
 		
 	}
 	
 	public boolean isZero(){
-		return false;
-		
+		switch(direction){
+		case Down:
+			return ( T_start + duration <= System.currentTimeMillis());	
+		default:
+			return false;
+		}
+			
 	}
 	
 	public int getTime(){
-		return 0;		
+		return new BigDecimal(((System.currentTimeMillis()-T_start)/1000)).intValueExact();
 	};
 }
