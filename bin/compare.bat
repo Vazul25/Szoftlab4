ebe81d90f86627d033515dc93d8b455bdb9ad243
demegis <tht_krisztian@sch.bme.hu>
2015-04-19 21:08:37 +0200

    Beadás #4

--- bin/compare.bat
+++ bin/compare.bat
@@ -1,17 +1,35 @@
-java comparer teszt1.txt elvart1.txt >> compareki1.txt
-java comparer teszt2.txt elvart2.txt >> compareki2.txt
-java comparer teszt3.txt elvart3.txt >> compareki3.txt
-java comparer teszt4.txt elvart4.txt >> compareki4.txt
-java comparer teszt5.txt elvart5.txt >> compareki5.txt
-java comparer teszt6.txt elvart6.txt >> compareki6.txt
-java comparer teszt7.txt elvart7.txt >> compareki7.txt
-java comparer teszt8.txt elvart8.txt >> compareki8.txt
-java comparer teszt9.txt elvart9.txt >> compareki9.txt
-java comparer teszt10.txt elvart10.txt >> compareki10.txt
-java comparer teszt11.txt elvart11.txt >> compareki11.txt
-java comparer teszt12.txt elvart12.txt >> compareki12.txt
-java comparer teszt13.txt elvart13.txt >> compareki13.txt
-java comparer teszt14.txt elvart14.txt >> compareki14.txt
-java comparer teszt15.txt elvart15.txt >> compareki15.txt
-java comparer teszt16.txt elvart16.txt >> compareki16.txt
-java comparer teszt17.txt elvart17.txt >> compareki17.txt
+echo Teszt1
+java osszehasonlito.comparer generalt_kimenetek\teszt1.txt elvart_kimenetek\elvart1.txt 
+echo Teszt2
+java osszehasonlito.comparer generalt_kimenetek\teszt2.txt elvart_kimenetek\elvart2.txt 
+echo Teszt3
+java osszehasonlito.comparer generalt_kimenetek\teszt3.txt elvart_kimenetek\elvart3.txt 
+echo Teszt4
+java osszehasonlito.comparer generalt_kimenetek\teszt4.txt elvart_kimenetek\elvart4.txt 
+echo Teszt5
+java osszehasonlito.comparer generalt_kimenetek\teszt5.txt elvart_kimenetek\elvart5.txt 
+echo Teszt6
+java osszehasonlito.comparer generalt_kimenetek\teszt6.txt elvart_kimenetek\elvart6.txt 
+echo Teszt7
+java osszehasonlito.comparer generalt_kimenetek\teszt7.txt elvart_kimenetek\elvart7.txt 
+echo Teszt8
+java osszehasonlito.comparer generalt_kimenetek\teszt8.txt elvart_kimenetek\elvart8.txt 
+echo Teszt9
+java osszehasonlito.comparer generalt_kimenetek\teszt9.txt elvart_kimenetek\elvart9.txt 
+echo Teszt10
+java osszehasonlito.comparer generalt_kimenetek\teszt10.txt elvart_kimenetek\elvart10.txt 
+echo Teszt11
+java osszehasonlito.comparer generalt_kimenetek\teszt11.txt elvart_kimenetek\elvart11.txt 
+echo Teszt12
+java osszehasonlito.comparer generalt_kimenetek\teszt12.txt elvart_kimenetek\elvart12.txt 
+echo Teszt13
+java osszehasonlito.comparer generalt_kimenetek\teszt13.txt elvart_kimenetek\elvart13.txt 
+echo Teszt14
+java osszehasonlito.comparer generalt_kimenetek\teszt14.txt elvart_kimenetek\elvart14.txt 
+echo Teszt15
+java osszehasonlito.comparer generalt_kimenetek\teszt15.txt elvart_kimenetek\elvart15.txt 
+echo Teszt16
+java osszehasonlito.comparer generalt_kimenetek\teszt16.txt elvart_kimenetek\elvart16.txt 
+echo Teszt17
+java osszehasonlito.comparer generalt_kimenetek\teszt17.txt elvart_kimenetek\elvart17.txt 
+pause
