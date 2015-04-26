90862e314b7e015e72aa925f4458fe8a0783a9ec
demegis <tht_krisztian@sch.bme.hu>
2015-04-26 21:21:46 +0200

    GUI generálva
    Funkcionalitás nincs meg

--- src/minor/GUI.java
+++ src/minor/GUI.java
@@ -1,5 +1,7 @@
 ﻿package minor;
 
+import java.io.IOException;
+
 import major.Phoebe;
 
 /**
@@ -40,8 +42,8 @@ public class GUI extends javax.swing.JFrame {
 	 * Funkció(ki hívja meg és mikor?):
 	 * 
 	 */
-	public GUI(Phoebe p){
-		game=p;
+	public GUI(){
+		initComponents();
 	}
 	
 	/*
@@ -52,18 +54,96 @@ public class GUI extends javax.swing.JFrame {
 	 * 
 	 */
 	private void initComponents(){
-		
+
+        jPanel1 = new javax.swing.JPanel();
+        gameName = new javax.swing.JLabel();
+        gameType = new javax.swing.JComboBox();
+        jatekmod = new javax.swing.JLabel();
+        limit = new javax.swing.JLabel();
+        limitField = new javax.swing.JTextField();
+        startButton = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        gameName.setText("Phoebe");
+
+        gameType.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Időlimit", "Körlimit" }));
+
+        jatekmod.setText("Játékmód");
+
+        limit.setText("Limit");
+
+        startButton.setText("Indítás");
+        startButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                startButtonActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(20, 20, 20)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jatekmod)
+                            .addComponent(limit))
+                        .addGap(25, 25, 25)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(limitField)
+                            .addComponent(gameType, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(49, 49, 49)
+                        .addComponent(startButton))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(69, 69, 69)
+                        .addComponent(gameName)))
+                .addContainerGap(40, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(23, 23, 23)
+                .addComponent(gameName)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(gameType, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jatekmod, javax.swing.GroupLayout.Alignment.TRAILING))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(limit)
+                    .addComponent(limitField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addComponent(startButton)
+                .addContainerGap(47, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+
+        pack();
 	}
 	
 	/*
-	 * createGameButtonActionPerformed függvény
+	 * startButtonActionPerformed függvény
 	 * Felelősség: A beállítások kiolvasása a megfelelő objektumokból, 
 	 * majd ezekkel a paraméterekkel a játék elindítása.
 	 * 
 	 * Funkció(ki hívja meg és mikor?): A CreateGame gomb megnyomása után hívódik meg.
 	 * 
-	 *//*
-	private void createGameButtonActionPerformed(java.awt.event.ActionEvent evt) throws IOException{
+	 */	
+	 private void startButtonActionPerformed(java.awt.event.ActionEvent evt) {
 		//...
 		//Beállítások lekérdezése
 		//...
@@ -77,11 +157,52 @@ public class GUI extends javax.swing.JFrame {
 		set.setStep(step);
 		//...
 		//Szál létrehozása, indítása
-		game = new Phoebe(set);
-		Thread t = new Thread(game);
-		t.start();
+		try {
+			game = new Phoebe(set);
+			Thread t = new Thread(game);
+			t.start();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		//...
-	}*/
+	    }
 
+	    /**
+	     * @param args the command line arguments
+	     */
+	    public static void main(String args[]) {
+	        try {
+	            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+	                if ("Nimbus".equals(info.getName())) {
+	                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+	                    break;
+	                }
+	            }
+	        } catch (ClassNotFoundException ex) {
+	            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+	        } catch (InstantiationException ex) {
+	            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+	        } catch (IllegalAccessException ex) {
+	            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+	        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+	            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+	        }
 
+	        /* Create and display the form */
+	        java.awt.EventQueue.invokeLater(new Runnable() {
+	            public void run() {
+	                new GUI().setVisible(true);
+	            }
+	        });
+	    }
+
+	    private javax.swing.JLabel gameName;
+	    private javax.swing.JComboBox gameType;
+	    private javax.swing.JPanel jPanel1;
+	    private javax.swing.JLabel jatekmod;
+	    private javax.swing.JLabel limit;
+	    private javax.swing.JTextField limitField;
+	    private javax.swing.JButton startButton;
 }
